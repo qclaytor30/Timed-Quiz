9735d5a75021d498354d624a13011408121176af
qclaytor30 <qclaytor30@gmail.com>
2022-12-14 14:28:41 -0500

    add quiz questions in js

---
+++ assets/js/quiz.js
@@ -0,0 +1,52 @@
+const quizQuestions = [
+    {
+        question:'Which one of the following also known as Conditional Expression:',
+        answerArray:
+            [
+            { answer:'Alternative to if-else', correct: false},
+            { answer:'Switch statement', correct: false},
+            { answer: 'If-then-else statement', correct: false},
+            { answer: 'immediate if', correct: true} 
+        ]
+    },
+    {
+        question: 'In JavaScript, what is a block of statement?',
+        answerArray:
+        [
+            { answer: 'Conditional block', correct: false},
+            { answer: 'block that combines a number of statements into a single compound statement', correct: true},
+            { answer: 'both conditional block and a single statement', correct: false},
+            { answer: 'block that contains a single statement', correct: false}
+    ]
+    },
+    {
+        question:'When interpreter encounters an empty statements, what it will do:',
+        answerArray:
+        [
+            { answer: 'Shows a warning', correct: false},
+            { answer: 'Prompts to complete the statement', correct: false},
+            { answer: 'Throws an error', correct: false},
+            { answer: 'Ignores the statements', correct: true}
+        ]
+    },
+    {
+        question: 'The "function" and " var" are known as:',
+        answerArray:
+        [
+            { answer: 'Keywords', correct: false},
+            { answer: 'Data types', correct: false},
+            { answer: 'Declaration statements', correct: true},
+            { answer: 'Prototypes', correct: false}
+        ]
+    },
+    {
+        question: 'Which one of the following is the correct way for calling the JavaScript code?',
+        answerArray:
+        [
+            { answer: 'Preprocessor', correct: false},
+            { answer: 'Triggering event', correct: false},
+            { answer: 'RMI', correct: false},
+            { answer: 'Function/Method', correct: true}
+        ]
+    }
+]
